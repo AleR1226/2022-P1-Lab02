eb986401fc76942dbab9859612f15f437462e599
Ale <alejandrina.roman@vtsd.com>
2022-09-15 08:12:38 -0400

    lab done

---
+++ Lab02v80.java
@@ -0,0 +1,30 @@
+// Lab02v80.java
+//The 80-point version of Lab 02 is an exercise in precisely copying a
+//provided program, followed by compiling the source code into bytecode
+//and executing the bytecode file.
+
+
+public class Lab02v80
+{
+    public static void main (String[] agrs)
+    {
+        System.out.println("Lab 02, 80 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.println("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("#  #####  #####    #####  #####  #  #    #  #####  #####");
+        System.out.println("#  #   #  #   #    #   #  #   #  #  ##  #     #    #");
+        System.out.println("#  #   #  #   #    #####  #   #  #  # # #     #    #####");
+        System.out.println("#  #   #  #   #    #      #   #  #  #  ##     #        #");
+        System.out.println("#  #####  #####    #      #####  #  #   #     #    #####");
+    }
+
+
+
+
+
+}
